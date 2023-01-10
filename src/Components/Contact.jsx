f2d5084745fa1d2b351479ff3676d68fe227e2c8
Ashish Saurav <ashishsaurav2410@gmail.com>
2023-01-10 12:34:02 +0530

    added update

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method="POST" action="https://getform.io/f/00cf513f-e9ac-4f70-bcbc-ad4ff711f407" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-pink-600 text-gray-300'>Contact</p>
-                <p className='text-gray-300 py-4'>//Submit the form below or shoot me an email- ashishsaurav2410@gmail.com</p>
+                <p className='text-gray-300 py-4'>"//Submit the form below or shoot me an email- ashishsaurav2410@gmail.com"</p>
             </div>
             <input className='bg-[#ccd6f6] p-2'  type="text" name="name"  placeholder='Name' />
             <input className='my-4 p-2 bg-[#ccd6f6]' type="email" name="email"  placeholder='E-Mail' />
