a6ea70fef0b0fa7819de4806cbee78ddc596fe48
Ashish Saurav <ashishsaurav2410@gmail.com>
2023-05-03 18:27:31 +0530

    updated work

--- src/Components/Work.jsx
+++ src/Components/Work.jsx
@@ -126,7 +126,7 @@ const Work = () => {
                 NewsMonkey
               </span>
               <div className="pt-8 text-center ">
-                <a href="newsappusingapi.netlify.app">
+                <a href="https://newsappusingapi.netlify.app/">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg ">
                     Demo
                   </button>
